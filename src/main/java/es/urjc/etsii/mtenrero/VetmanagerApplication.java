5d0fdee68a66421d25ad3e676e3e1b650124b844
Marcos Tenrero <m.tenrero@alumnos.urjc.es>
2017-01-25 01:07:01 +0100

    Basic mvn & Spring project structure

---
+++ src/main/java/es/urjc/etsii/mtenrero/VetmanagerApplication.java
@@ -0,0 +1,12 @@
+package es.urjc.etsii.mtenrero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VetmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VetmanagerApplication.class, args);
+	}
+}
