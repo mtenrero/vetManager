003d8c9753163ef046e86c2f301eb74b6b0be9ad
Marcos Tenrero <marcos.95tm@gmail.com>
2017-01-28 18:24:28 +0100

    Created basic Dashboard Navigation

---
+++ src/main/java/es/urjc/etsii/mtenrero/DashboardController.java
@@ -0,0 +1,16 @@
+package es.urjc.etsii.mtenrero;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by mtenrero on 28/01/2017.
+ */
+@Controller
+public class DashboardController {
+    @RequestMapping("/dashboard")
+    public String getLanding(Model model) {
+        return "dashboard";
+    }
+}
