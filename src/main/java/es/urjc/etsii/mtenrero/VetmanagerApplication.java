a0f999ab83d713392caaf083d71b472f1b58322e
Marcos Tenrero <m.tenrero@alumnos.urjc.es>
2017-03-20 16:54:15 +0100

    Implemented SSL encryption with Apache + Let's encrypt certificate
    In order to run in local instance: mvn spring-boot:run

--- src/main/java/es/urjc/etsii/mtenrero/VetmanagerApplication.java
+++ src/main/java/es/urjc/etsii/mtenrero/VetmanagerApplication.java
@@ -1,9 +1,13 @@
 package es.urjc.etsii.mtenrero;
 
+import org.apache.catalina.connector.Connector;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class VetmanagerApplication extends SpringBootServletInitializer {
@@ -17,5 +21,21 @@ public class VetmanagerApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(VetmanagerApplication.class, args);
+	}
+
+	@Bean
+	public EmbeddedServletContainerFactory servletContainer() {
+
+		TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory();
+
+		Connector ajpConnector = new Connector("AJP/1.3");
+		ajpConnector.setProtocol("AJP/1.3");
+		ajpConnector.setPort(9090);
+		ajpConnector.setSecure(false);
+		ajpConnector.setAllowTrace(false);
+		ajpConnector.setScheme("http");
+		tomcat.addAdditionalTomcatConnectors(ajpConnector);
+
+		return tomcat;
 	}
 }
